b32e7db74f8dd8b51db62b0e8b2322beb32839a3
Danish-Ehsan <danish.ehsan321@gmail.com>
2023-08-03 16:28:59 -0400

    Added ComponentC with repeating animation

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
                     Switch Component
                 </button>
             </div>
+            <ComponentC />
         </>
     );
 }
@@ -48,6 +49,25 @@ function ComponentB() {
             transition={{ duration: 0.5 }}
         >
             Component B
+        </motion.div>
+    );
+}
+
+function ComponentC() {
+    return (
+        <motion.div
+            className="component purple"
+            animate={{
+                scale: [0.8, 1, 0.8],
+                y: [0, 100, 0]
+            }}
+            transition={{
+                duration: 2,
+                ease: "easeInOut",
+                repeat: 1
+            }}
+        >
+            Component C
         </motion.div>
     );
 }
